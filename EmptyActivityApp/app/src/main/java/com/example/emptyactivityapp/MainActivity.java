3e5afc5d20c15df93941ab4edb733d78feeaf4f7
tarunraghav226 <tarun.raghav226@gmail.com>
2020-01-14 23:39:20 +0530

    added buttons and rating bar

--- EmptyActivityApp/app/src/main/java/com/example/emptyactivityapp/MainActivity.java
+++ EmptyActivityApp/app/src/main/java/com/example/emptyactivityapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.emptyactivityapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+
+    void loadConstraintLayout(View view) {
+        setContentView(R.layout.activity_main);
+    }
+
+    void loadTableLayout(View view) {
+        //setContentView(R.layout.my_table_layout);
     }
 }
