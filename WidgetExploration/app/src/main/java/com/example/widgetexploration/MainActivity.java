1293672b40e3890b4916904fafa21075d8149e40
tarunraghav226 <tarun.raghav226@gmail.com>
2020-02-11 23:56:35 +0530

    Added code for cbTransparency and cbTint button.

--- WidgetExploration/app/src/main/java/com/example/widgetexploration/MainActivity.java
+++ WidgetExploration/app/src/main/java/com/example/widgetexploration/MainActivity.java
@@ -1,6 +1,17 @@
 package com.example.widgetexploration;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +20,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.exploration_layout);
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        final Button button = (Button) findViewById(R.id.button);
+        final TextClock tClock = (TextClock) findViewById(R.id.textClock);
+        final CheckBox cbTransparency = (CheckBox) findViewById(R.id.checkBoxTransparency);
+        final CheckBox cbTint = (CheckBox) findViewById(R.id.checkBoxTint);
+        final CheckBox cbReSize = (CheckBox) findViewById(R.id.checkBoxReSize);
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setVisibility(View.INVISIBLE);
+
+        cbTransparency.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (cbTransparency.isChecked()) {
+                            imageView.setAlpha(.1f);
+                        } else {
+                            imageView.setAlpha(1f);
+                        }
+                    }
+                }
+        );
+
+        cbTint.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (cbTint.isChecked()) {
+                            imageView.setColorFilter(Color.argb(150, 255, 0, 0));
+                        } else {
+                            imageView.setColorFilter(Color.argb(0, 0, 0, 0));
+                        }
+                    }
+                }
+        );
     }
 }
