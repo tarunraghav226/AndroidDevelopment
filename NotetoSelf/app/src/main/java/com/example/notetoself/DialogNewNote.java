9f6be05e9f59250452fcd6386d7e7f20301ad819
tarunraghav226 <tarun.raghav226@gmail.com>
2020-02-25 23:51:19 +0530

    Added references of UI element in DialogNewNote class.

--- NotetoSelf/app/src/main/java/com/example/notetoself/DialogNewNote.java
+++ NotetoSelf/app/src/main/java/com/example/notetoself/DialogNewNote.java
@@ -1,4 +1,33 @@
 package com.example.notetoself;
 
-public class DialogNewNote {
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.fragment.app.DialogFragment;
+
+public class DialogNewNote extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View dialogView = inflater.inflate(R.layout.dialog_new_note, null);
+
+        final EditText editTitle = (EditText) dialogView.findViewById(R.id.editTitle);
+        final EditText editDescription = (EditText) dialogView.findViewById(R.id.editDescription);
+        final CheckBox checkBoxIdea = (CheckBox) dialogView.findViewById(R.id.checkBoxIdea);
+        final CheckBox checkBoxToDo = (CheckBox) dialogView.findViewById(R.id.checkBoxToDo);
+        final CheckBox checkBoxImportant = (CheckBox) dialogView.findViewById(R.id.checkBoxImportant);
+        Button btnCancel = (Button) dialogView.findViewById(R.id.btnCancel);
+        Button btnOK = (Button) dialogView.findViewById(R.id.btnOK);
+
+    }
 }
