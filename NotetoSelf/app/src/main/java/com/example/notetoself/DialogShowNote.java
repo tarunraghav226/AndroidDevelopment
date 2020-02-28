21513754e1d2046828bb52691432222128433e65
tarunraghav226 <tarun.raghav226@gmail.com>
2020-02-28 23:56:27 +0530

    Added sendNoteSelected method in DialodShowNote class.

--- NotetoSelf/app/src/main/java/com/example/notetoself/DialogShowNote.java
+++ NotetoSelf/app/src/main/java/com/example/notetoself/DialogShowNote.java
@@ -1,4 +1,17 @@
 package com.example.notetoself;
 
+import android.app.Dialog;
+import android.os.Bundle;
+
 public class DialogShowNote {
+    private Note mNote;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+    }
+
+    public void sendNoteSelected(Note noteSelected) {
+        mNote = noteSelected;
+    }
 }
