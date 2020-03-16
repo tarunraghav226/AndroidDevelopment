fd0802095481646e248266f40ed9c5a04c614d58
tarunraghav226 <tarun.raghav226@gmail.com>
2020-03-16 23:55:36 +0530

    Added code in Settings activity.

--- NotetoSelf/app/src/main/java/com/example/notetoself/SettingsActivity.java
+++ NotetoSelf/app/src/main/java/com/example/notetoself/SettingsActivity.java
@@ -2,13 +2,48 @@ package com.example.notetoself;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private boolean showDividers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        preferences = getSharedPreferences("Note To Self",MODE_PRIVATE);
+        editor = preferences.edit();
+        showDividers = preferences.getBoolean("dividers",true);
+
+        Switch switch1 = (Switch)findViewById(R.id.switch1);
+        switch1.setChecked(showDividers);
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    editor.putBoolean("dividers",true);
+                    showDividers=true;
+                }
+                else{
+                    editor.putBoolean("dividers",false);
+                    showDividers=false;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        editor.commit();
     }
 }
