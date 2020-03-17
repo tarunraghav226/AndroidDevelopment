c21400c947d52d637fa0c901ba9d1c9f17d1815e
tarunraghav226 <tarun.raghav226@gmail.com>
2020-03-17 22:58:23 +0530

    Added load method in JSONSerializer class.

--- NotetoSelf/app/src/main/java/com/example/notetoself/JSONSerializer.java
+++ NotetoSelf/app/src/main/java/com/example/notetoself/JSONSerializer.java
@@ -4,11 +4,17 @@ import android.content.Context;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONTokener;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JSONSerializer {
@@ -38,5 +44,32 @@ public class JSONSerializer {
             if(writer!=null)
                 writer.close();
         }
+    }
+
+    public ArrayList<Note> load() throws IOException, JSONException{
+        ArrayList<Note> list = new ArrayList<>();
+        BufferedReader reader = null;
+        try{
+            InputStream in = context.openFileInput(fileName);
+            reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder jsonString = new StringBuilder();
+            String line = null;
+
+            while((line = reader.readLine())!=null)
+                jsonString.append(line);
+
+            JSONArray jsonArray = (JSONArray) new JSONTokener(jsonString.toString()).nextValue();
+
+            for(int i=0;i<jsonArray.length();i++)
+                list.add(new Note(jsonArray.getJSONObject(i)));
+        }
+        catch (FileNotFoundException e){
+
+        }
+        finally {
+            if(reader!=null)
+                reader.close();
+        }
+        return list;
     }
 }
