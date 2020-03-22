224d157366f27a0155bbefb638390d906e5278d2
tarunraghav226 <tarun.raghav226@gmail.com>
2020-03-22 23:10:36 +0530

    Created a simple imageView.

--- CanvasDemo/app/src/main/java/com/example/canvasdemo/MainActivity.java
+++ CanvasDemo/app/src/main/java/com/example/canvasdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.canvasdemo;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -17,5 +18,28 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        int widthInPixel = 800;
+        int heightInPixel = 600;
+
+        bitmap = Bitmap.createBitmap(widthInPixel, heightInPixel, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        imageView = new ImageView(this);
+        paint = new Paint();
+
+        canvas.drawColor(Color.argb(255, 0, 0, 255));
+
+        paint.setTextSize(100);
+        paint.setColor(Color.argb(255, 255, 255, 255));
+
+        canvas.drawText("Hello Tarun", 100, 100, paint);
+
+        paint.setColor(Color.argb(255, 212, 207, 62));
+
+        canvas.drawCircle(400, 250, 100, paint);
+
+        imageView.setImageBitmap(bitmap);
+
+        setContentView(imageView);
     }
 }
