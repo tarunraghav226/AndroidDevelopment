36a93bf842ca8f5abad99e1957abe735af10ff93
tarunraghav226 <tarun.raghav226@gmail.com>
2020-03-23 20:25:21 +0530

    Removed bug.

--- InfoTime/app/src/main/java/com/example/infotime/MainActivity.java
+++ InfoTime/app/src/main/java/com/example/infotime/MainActivity.java
@@ -12,11 +12,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         //code to hide title and title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
 
         WebView webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(new ClientView());
