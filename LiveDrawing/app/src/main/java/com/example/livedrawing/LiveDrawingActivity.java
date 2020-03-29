7392f4e23ae93bb9c58a340a601b3b483612f3e8
tarunraghav226 <tarun.raghav226@gmail.com>
2020-03-29 11:49:44 +0530

    Added life methods.

--- LiveDrawing/app/src/main/java/com/example/livedrawing/LiveDrawingActivity.java
+++ LiveDrawing/app/src/main/java/com/example/livedrawing/LiveDrawingActivity.java
@@ -1,14 +1,34 @@
 package com.example.livedrawing;
 
+import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class LiveDrawingActivity extends Activity {
 
-public class LiveDrawingActivity extends AppCompatActivity {
+    private LiveDrawingView liveDrawingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        liveDrawingView = new LiveDrawingView(this, size.x, size.y);
+
+        setContentView(liveDrawingView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
