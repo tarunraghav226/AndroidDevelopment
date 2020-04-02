96bafc65382c488dff62f3f278e21296ce031059
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-02 10:44:59 +0530

    Added member variables.

--- LiveDrawing/app/src/main/java/com/example/livedrawing/LiveDrawingView.java
+++ LiveDrawing/app/src/main/java/com/example/livedrawing/LiveDrawingView.java
@@ -1,4 +1,27 @@
 package com.example.livedrawing;
 
-public class LiveDrawingView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class LiveDrawingView extends SurfaceView {
+
+    private final boolean DEBBUGING = true;
+    private final int MILLI_SECONDS = 1000;
+    private SurfaceHolder ourHolder;
+    private Canvas canvas;
+    private Paint paint;
+    private long FPS;
+    private int screenX;
+    private int screenY;
+
+    private int fontSize;
+    private int fontMargin;
+
+    public LiveDrawingView(Context context, int x, int y) {
+        super(context);
+    }
+
 }
