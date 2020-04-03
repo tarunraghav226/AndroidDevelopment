bda877b4838a0c4bffff0c1244065c1915129825
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-03 21:09:19 +0530

    Added member variable and constructor in Particle class.

--- LiveDrawing/app/src/main/java/com/example/livedrawing/Particle.java
+++ LiveDrawing/app/src/main/java/com/example/livedrawing/Particle.java
@@ -1,4 +1,16 @@
 package com.example.livedrawing;
 
+import android.graphics.PointF;
+
 public class Particle {
+    private PointF velocity;
+    private PointF position;
+
+    public Particle(PointF direction) {
+        velocity = new PointF();
+        position = new PointF();
+
+        velocity.x = direction.x;
+        velocity.y = direction.y;
+    }
 }
