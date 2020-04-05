c1d6c31fee5250b61d5144556007d196a4c4a0d8
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-05 17:18:50 +0530

    Added onCreate and onCreateView method.

--- SimpleFragment/app/src/main/java/com/example/simplefragment/SimpleFragment.java
+++ SimpleFragment/app/src/main/java/com/example/simplefragment/SimpleFragment.java
@@ -1,6 +1,39 @@
 package com.example.simplefragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
 
 public class SimpleFragment extends Fragment {
+
+    private String myString;
+    private Button myButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        myString = "Hello world, It's Tarun here.";
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_layout, container, false);
+
+        myButton = (Button) view.findViewById(R.id.fragmentHolder);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), myString, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+
 }
