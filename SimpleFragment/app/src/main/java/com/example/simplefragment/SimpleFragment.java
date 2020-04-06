ccbf29484962c5cd5034e34f8e3be2a1ddcec65c
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-06 17:26:33 +0530

    Init commit.

--- SimpleFragment/app/src/main/java/com/example/simplefragment/SimpleFragment.java
+++ SimpleFragment/app/src/main/java/com/example/simplefragment/SimpleFragment.java
@@ -25,7 +25,7 @@ public class SimpleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_layout, container, false);
 
-        myButton = (Button) view.findViewById(R.id.fragmentHolder);
+        myButton = (Button) view.findViewById(R.id.button);
 
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
