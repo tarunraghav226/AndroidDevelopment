a47e9c505691ebb196e77b0922b19ebac3b0c5d4
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-06 17:42:37 +0530

    Added member variables and overridden methods.

---
+++ ImagePager/app/src/main/java/com/example/imagepager/ImagePagerAdapter.java
@@ -0,0 +1,26 @@
+package com.example.imagepager;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ImagePagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private int[] images;
+    private LayoutInflater inflater;
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return false;
+    }
+}
