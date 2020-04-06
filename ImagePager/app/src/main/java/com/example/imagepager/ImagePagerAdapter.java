1865bb8c25a267566a505238e960da26a46d659d
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-06 17:51:43 +0530

    Completed ImagePagerAdapter class.

--- ImagePager/app/src/main/java/com/example/imagepager/ImagePagerAdapter.java
+++ ImagePager/app/src/main/java/com/example/imagepager/ImagePagerAdapter.java
@@ -3,9 +3,13 @@ package com.example.imagepager;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class ImagePagerAdapter extends PagerAdapter {
 
@@ -13,14 +17,35 @@ public class ImagePagerAdapter extends PagerAdapter {
     private int[] images;
     private LayoutInflater inflater;
 
+    public ImagePagerAdapter(Context context, int[] images) {
+        this.context = context;
+        this.images = images;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return images.length;
     }
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return false;
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        ImageView image;
+        View itemView = inflater.inflate(R.layout.pager_item, container, false);
+        image = (ImageView) itemView.findViewById(R.id.imageView);
+        image.setImageResource(images[position]);
+        ((ViewPager) container).addView(itemView);
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((RelativeLayout) object);
     }
 }
