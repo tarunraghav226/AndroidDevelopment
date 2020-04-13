4c3c4fd611e558ba8ec68dc0fbffa03576d6c132
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-13 20:03:44 +0530

    Added code to delete data from database.

--- AgeDatabase/app/src/main/java/com/example/agedatabase/DeleteFragment.java
+++ AgeDatabase/app/src/main/java/com/example/agedatabase/DeleteFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,6 +13,20 @@ public class DeleteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.content_delete, container, false);
+
+        final DataManager dataManager = new DataManager(getActivity());
+
+        Button btnDelete = (Button) v.findViewById(R.id.btnDelete);
+
+        final EditText editDelete = (EditText) v.findViewById(R.id.editDelete);
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataManager.delete(editDelete.getText().toString());
+            }
+        });
+
         return v;
     }
 }
