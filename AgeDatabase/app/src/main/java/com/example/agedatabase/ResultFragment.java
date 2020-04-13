4b6e27fa91b086a6c295347d11cd41682b20a635
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-13 20:20:40 +0530

    Added code to search all result data from database.

--- AgeDatabase/app/src/main/java/com/example/agedatabase/ResultFragment.java
+++ AgeDatabase/app/src/main/java/com/example/agedatabase/ResultFragment.java
@@ -1,9 +1,11 @@
 package com.example.agedatabase;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,6 +13,19 @@ public class ResultFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.content_result, container, false);
+
+        DataManager dataManager = new DataManager(getActivity());
+        TextView textResult = (TextView) v.findViewById(R.id.textResults);
+        Cursor c = dataManager.selectAll();
+
+        String list = "";
+
+        while (c.moveToNext()) {
+            list += (c.getString(1) + "-->" + c.getString(2) + "\n");
+        }
+
+        textResult.setText(list);
+
         return v;
     }
 }
