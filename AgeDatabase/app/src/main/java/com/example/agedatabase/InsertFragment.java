50698539464e168f92b88463fd118e48f82fdb28
tarunraghav226 <tarun.raghav226@gmail.com>
2020-04-13 19:57:39 +0530

    Added code to insert data in database.

--- AgeDatabase/app/src/main/java/com/example/agedatabase/InsertFragment.java
+++ AgeDatabase/app/src/main/java/com/example/agedatabase/InsertFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,6 +13,18 @@ public class InsertFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.content_insert, container, false);
+        final DataManager dataManager = new DataManager(getActivity());
+        Button btnInsert = v.findViewById(R.id.btnInsert);
+        final EditText editName = v.findViewById(R.id.editName);
+        final EditText editAge = v.findViewById(R.id.editAge);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataManager.insert(editName.getText().toString(), editAge.getText().toString());
+            }
+        });
+
         return v;
     }
 }
