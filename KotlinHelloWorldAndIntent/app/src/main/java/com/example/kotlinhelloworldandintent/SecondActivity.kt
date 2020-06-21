4481700294680cd72a09cf160606563e2cf99f94
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-21 20:59:10 +0530

    Added SecondActivity code.

--- KotlinHelloWorldAndIntent/app/src/main/java/com/example/kotlinhelloworldandintent/SecondActivity.kt
+++ KotlinHelloWorldAndIntent/app/src/main/java/com/example/kotlinhelloworldandintent/SecondActivity.kt
@@ -2,9 +2,14 @@ package com.example.kotlinhelloworldandintent
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val username: String? = intent.extras?.get("username").toString()
+
+        //second activity text view
+        secondTxtMsg.text = username
     }
 }
