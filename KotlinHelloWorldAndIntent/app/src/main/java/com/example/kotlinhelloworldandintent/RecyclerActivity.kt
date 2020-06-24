2a16047b909d0dd930e43f348a09b9ab13f8c985
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-24 13:07:07 +0530

    Added recycler activity.

---
+++ KotlinHelloWorldAndIntent/app/src/main/java/com/example/kotlinhelloworldandintent/RecyclerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinhelloworldandintent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecyclerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler)
+    }
+}
