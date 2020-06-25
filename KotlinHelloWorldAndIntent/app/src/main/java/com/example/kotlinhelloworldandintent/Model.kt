0a72d23fb706facee5b3634bd2ee0e69a0a6057b
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-25 18:50:49 +0530

    Added model class.

---
+++ KotlinHelloWorldAndIntent/app/src/main/java/com/example/kotlinhelloworldandintent/Model.kt
@@ -0,0 +1,32 @@
+package com.example.kotlinhelloworldandintent
+
+data class Person(val name: String, val Address: String)
+
+object DataSupplier {
+    val list = listOf(
+        Person("Tarun", "Avas Vikas"),
+        Person("Varun", "dasf"),
+        Person("fsd", "asdf"),
+        Person("sdfa", "sadf"),
+        Person("safda", "asdf"),
+        Person("sdfaw", "asdf"),
+        Person("werwsdf", "qwerw"),
+        Person("asdf", "asdfa"),
+        Person("Tarun", "Avas Vikas"),
+        Person("Varun", "dasf"),
+        Person("fsd", "asdf"),
+        Person("sdfa", "sadf"),
+        Person("safda", "asdf"),
+        Person("sdfaw", "asdf"),
+        Person("werwsdf", "qwerw"),
+        Person("asdf", "asdfa"),
+        Person("Tarun", "Avas Vikas"),
+        Person("Varun", "dasf"),
+        Person("fsd", "asdf"),
+        Person("sdfa", "sadf"),
+        Person("safda", "asdf"),
+        Person("sdfaw", "asdf"),
+        Person("werwsdf", "qwerw"),
+        Person("asdf", "asdfa")
+    )
+}
