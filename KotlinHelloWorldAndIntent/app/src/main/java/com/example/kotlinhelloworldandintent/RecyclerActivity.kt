65edf6f9a9404e6c195733fe2f1cb4cf093a480c
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-25 18:50:17 +0530

    Added RecyclerActivity.

--- KotlinHelloWorldAndIntent/app/src/main/java/com/example/kotlinhelloworldandintent/RecyclerActivity.kt
+++ KotlinHelloWorldAndIntent/app/src/main/java/com/example/kotlinhelloworldandintent/RecyclerActivity.kt
@@ -2,10 +2,17 @@ package com.example.kotlinhelloworldandintent
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_recycler.*
 
 class RecyclerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler)
+
+        val layout = LinearLayoutManager(this)
+        layout.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layout
+        recyclerView.adapter = MyAdapter(this, DataSupplier.list)
     }
 }
