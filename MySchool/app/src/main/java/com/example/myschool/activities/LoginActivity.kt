bbd5b303a24972f5303baa8c6dc33a45f0a964fe
tarunraghav226 <tarun.raghav226@gmail.com>
2020-07-07 22:34:18 +0530

    Added navigation drawer.

--- MySchool/app/src/main/java/com/example/myschool/activities/LoginActivity.kt
+++ MySchool/app/src/main/java/com/example/myschool/activities/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.example.myschool.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myschool.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, NavigationDrawerActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
