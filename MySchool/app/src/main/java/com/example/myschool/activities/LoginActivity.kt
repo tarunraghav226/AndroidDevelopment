dfd7c5e63386479c26eec77d881c80b9826d1833
tarunraghav226 <tarun.raghav226@gmail.com>
2020-08-01 22:54:34 +0530

    Updated login functionality.

--- MySchool/app/src/main/java/com/example/myschool/activities/LoginActivity.kt
+++ MySchool/app/src/main/java/com/example/myschool/activities/LoginActivity.kt
@@ -2,7 +2,6 @@ package com.example.myschool.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +9,7 @@ import com.example.myschool.R
 import com.example.myschool.model.UserAuthenticationToken
 import com.example.myschool.services.SchoolService
 import com.example.myschool.services.ServiceBuilder
+import com.example.myschool.services.TokenPreference
 import kotlinx.android.synthetic.main.activity_login.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -22,7 +22,9 @@ class LoginActivity : AppCompatActivity() {
 
         logInProgressBar.visibility = View.INVISIBLE
         loginButton.setOnClickListener {
-            loginButton.isClickable = false
+            loginUserID.isEnabled = false
+            loginPassword.isEnabled = false
+            loginButton.isEnabled = false
             logInProgressBar.visibility = View.VISIBLE;
             val studentService = ServiceBuilder.buildService(SchoolService::class.java)
             val reqCall = studentService.authorizeUser(
@@ -31,22 +33,30 @@ class LoginActivity : AppCompatActivity() {
             )
 
             reqCall.enqueue(object : Callback<UserAuthenticationToken> {
+
+                //function called when a request is successfully made to a server
                 override fun onResponse(
                     call: Call<UserAuthenticationToken>,
                     response: Response<UserAuthenticationToken>
                 ) {
+                    //setting visibility of progress bar
                     logInProgressBar.visibility = View.INVISIBLE
+
+                    //checking response status
                     if (response.isSuccessful) {
                         val intent =
                             Intent(applicationContext, NavigationDrawerActivity::class.java)
-                        intent.putExtra("token", response.body()!!.token)
-                        Log.i("info", response.body()!!.token)
+                        val tokenPref = TokenPreference(applicationContext)
+                        tokenPref.setToken(response.body()!!.token)
                         Toast.makeText(this@LoginActivity, "Login Successful", Toast.LENGTH_SHORT)
                             .show()
                         startActivity(intent)
+
                         finish()
                     } else {
-                        loginButton.isClickable = true
+                        loginButton.isEnabled = true
+                        loginUserID.isEnabled = true
+                        loginPassword.isEnabled = true
                         Toast.makeText(
                             this@LoginActivity,
                             "Wrong Username or Password",
@@ -56,8 +66,11 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
 
+                //function is called when some problem occurred during request
                 override fun onFailure(call: Call<UserAuthenticationToken>, t: Throwable) {
-                    loginButton.isClickable = true
+                    loginButton.isEnabled = true
+                    loginUserID.isEnabled = true
+                    loginPassword.isEnabled = true
                     logInProgressBar.visibility = View.INVISIBLE
                     Toast.makeText(this@LoginActivity, "Error Occurred", Toast.LENGTH_SHORT).show()
                 }
