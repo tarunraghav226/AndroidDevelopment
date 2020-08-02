31e4a212bab838dbedbda8d39bc4eca4758da8b2
tarunraghav226 <tarun.raghav226@gmail.com>
2020-08-02 14:07:54 +0530

    Updated Authenticated Service interface.

--- MySchool/app/src/main/java/com/example/myschool/services/AuthenticatedService.kt
+++ MySchool/app/src/main/java/com/example/myschool/services/AuthenticatedService.kt
@@ -1,10 +1,23 @@
 package com.example.myschool.services
 
 import com.example.myschool.model.Student
+import com.example.myschool.model.StudentSearchResult
+import com.example.myschool.model.TeacherSearchResult
 import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface AuthenticatedService {
     @GET("/api/dashboard/")
     fun getUserDetails(): Call<Student>
+
+    @POST("/api/search/")
+    @FormUrlEncoded
+    fun searchStudent(@Field("search_whom") search_whom: String, @Field("id") id: String): Call<StudentSearchResult>
+
+    @POST("/api/search/")
+    @FormUrlEncoded
+    fun searchTeacher(@Field("search_whom") search_whom: String, @Field("id") id: String): Call<TeacherSearchResult>
 }
